69e16156fdbff0008373dbe563ec6d763cd34ec0
Srikannan-E <srik.developer@gmail.com>
2026-05-14 20:37:32 +0530

    SQL file updated

--- src/main/resources/db/migration/V2__add_category_to_decision_queries.sql
+++ src/main/resources/db/migration/V2__add_category_to_decision_queries.sql
@@ -1,2 +1,2 @@
 -- Add category column to decision_queries
-ALTER TABLE decision_queries ADD COLUMN category VARCHAR(200) NOT NULL DEFAULT '';
+ALTER TABLE decision_queries ADD COLUMN IF NOT EXISTS category VARCHAR(200) NOT NULL DEFAULT '';
